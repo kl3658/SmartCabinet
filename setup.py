d0b3addda877ba2f328b445f6b14525e9321157c
Kent Li <kl3658@nyu.edu>
2023-02-01 13:28:41 -0500

    Added stub software library, see #4

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='SmartCabinet',
+      version='1.0',
+      description='CompE Stub Library',
+      author='Kent Li',
+      author_email='kl3658@nyu.edu',
+      url='https://github.com/kl3658/SmartCabinet',
+      py_modules=['smartcabinet'],
+)
---
+++ smartcabinet.py
@@ -0,0 +1,3 @@
+def info():
+    '''Prints a basic library description'''
+    print('Software library for the SmartCabinet project.')
