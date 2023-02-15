68beab0d0ada2f971d0ab5d32b3fe5f019aa95bf
Kent Li <kl3658@nyu.edu>
2023-02-15 13:18:07 -0500

    Connectivity testing for pressure pad, see #13

--- smartcabinet.py
+++ smartcabinet.py
@@ -1,4 +1,7 @@
 import random
+import spidev
+import time
+import RPi.GPIO as GPIO
 
 def info():
     '''Prints a basic library description'''
@@ -19,7 +22,34 @@ def identifyFace():
     
 def takePictureOfFace():
     faceThere = identifyFace()
-    if faceThere = True:
+    if faceThere == True:
         print("Photo taken!")
     else:
         print("No photo taken...")
+
+
+#Define Variables
+delay = 0.5
+pad_channel = 0
+
+#Create SPI
+spi = spidev.SpiDev()
+spi.open(0, 0)
+spi.max_speed_hz=1000000
+
+def readadc(adcnum):
+    # read SPI data
+    if adcnum > 7 or adcnum < 0:
+        return -1
+    r = spi.xfer2([1, 8 + adcnum << 4, 0])
+    data = ((r[1] & 3) << 8) + r[2]
+    return data
+
+try:
+    while True:
+        pad_value = readadc(pad_channel)
+        print("---------------------------------------")
+        print("Pressure Pad Value: %d" % pad_value)
+        time.sleep(delay)
+except KeyboardInterrupt:
+    pass
