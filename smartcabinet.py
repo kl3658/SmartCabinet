4152eab7756506c7792e2c5125eb38d4b2e9c347
Kent Li <kl3658@nyu.edu>
2023-02-15 13:35:42 -0500

    Commented out resistive band code since we are going to be using the load cell, see #17

--- smartcabinet.py
+++ smartcabinet.py
@@ -28,28 +28,28 @@ def takePictureOfFace():
         print("No photo taken...")
 
 
-#Define Variables
-delay = 0.5
-pad_channel = 0
+# Define Variables
+# delay = 0.5
+# pad_channel = 0
 
-#Create SPI
-spi = spidev.SpiDev()
-spi.open(0, 0)
-spi.max_speed_hz=1000000
+# Create SPI
+# spi = spidev.SpiDev()
+# spi.open(0, 0)
+# spi.max_speed_hz=1000000
 
-def readadc(adcnum):
-    # read SPI data
-    if adcnum > 7 or adcnum < 0:
-        return -1
-    r = spi.xfer2([1, 8 + adcnum << 4, 0])
-    data = ((r[1] & 3) << 8) + r[2]
-    return data
+# def readadc(adcnum):
+#    # read SPI data
+#    if adcnum > 7 or adcnum < 0:
+#        return -1
+#    r = spi.xfer2([1, 8 + adcnum << 4, 0])
+#    data = ((r[1] & 3) << 8) + r[2]
+#   return data
 
-try:
-    while True:
-        pad_value = readadc(pad_channel)
-        print("---------------------------------------")
-        print("Pressure Pad Value: %d" % pad_value)
-        time.sleep(delay)
-except KeyboardInterrupt:
-    pass
+# try:
+#    while True:
+#        pad_value = readadc(pad_channel)
+#        print("---------------------------------------")
+#        print("Pressure Pad Value: %d" % pad_value)
+#        time.sleep(delay)
+# except KeyboardInterrupt:
+#    pass
