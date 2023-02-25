b57750ebf5a1bb0b951ddd9c978c03c630bd1fda
Tommy Martinez <UGFruT@gmail.com>
2023-02-24 23:38:11 -0500

    Commented out unnecessary test code. Will keep in case.

--- demo.py
+++ demo.py
@@ -8,6 +8,6 @@ smartcabinet.turnOnCamera()
 smartcabinet.useCamera()
 
 # We could test this function out 3 times, which calls identifyFace in turn
-smartcabinet.savePhotoToFile(1)
-smartcabinet.savePhotoToFile(2)
-smartcabinet.savePhotoToFile(3)
+#smartcabinet.savePhotoToFile(1)
+#smartcabinet.savePhotoToFile(2)
+#smartcabinet.savePhotoToFile(3)
