bc902bb0e3bcaf3095f1aea4f6d127c7a764e5a7
Tommy Martinez <UGFruT@gmail.com>
2023-03-12 03:02:48 -0400

    Minor fixes

--- smartcabinet.py
+++ smartcabinet.py
@@ -307,8 +307,9 @@ servoPIN = 17
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(servoPIN, GPIO.OUT)
 
-p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
-p.start(2.5) # Initialization
+p = GPIO.PWM(servoPIN, 50)      # GPIO 17 for PWM with 50Hz
+p.start(2.5)                    # Initialization
+
 try:
     while True:
         p.ChangeDutyCycle(5)
