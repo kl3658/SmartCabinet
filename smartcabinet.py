ab568f39112adbf65bbfbd6fdb0d6d717a6aad58
Tommy Martinez <UGFruT@gmail.com>
2023-04-26 13:35:19 -0400

    Added code for RFID, and changed keypad pinout.

--- smartcabinet.py
+++ smartcabinet.py
@@ -1,6 +1,7 @@
 from global_ import camera
 from fractions import Fraction
 from tmlib import *
+from mfrc522 import SimpleMFRC522
 import random
 import math
 import spidev
@@ -198,6 +199,110 @@ try:
 except KeyboardInterrupt:
     pass
 
+# Keypad part goes here. Remember to somehow get them into functions and even have properties
+# called via functions like with the picamera stuff. Also a password system. GPIO Pins are shown for the keypad
+
+R1 = 13
+R2 = 19
+R3 = 26
+R4 = 12
+
+C1 = 16
+C2 = 20
+C3 = 21
+
+# Initialize the GPIO pins
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(R1, GPIO.OUT)
+GPIO.setup(R2, GPIO.OUT)
+GPIO.setup(R3, GPIO.OUT)
+GPIO.setup(R4, GPIO.OUT)
+
+# Make sure to configure the input pins to use the internal pull-down resistors
+
+GPIO.setup(C1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(C2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(C3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+# The readLine function implements the procedure discussed in the article
+# It sends out a single pulse to one of the rows of the keypad
+# and then checks each column for changes
+# If it detects a change, the user pressed the button that connects the given line
+# to the detected column
+
+def readLine(line, characters):
+    GPIO.output(line, GPIO.HIGH)
+    if(GPIO.input(C1) == 1):
+        print(characters[0])
+    if(GPIO.input(C2) == 1):
+        print(characters[1])
+    if(GPIO.input(C3) == 1):
+        print(characters[2])
+    GPIO.output(line, GPIO.LOW)
+
+try:
+    while True:
+        # call the readLine function for each row of the keypad
+        readLine(L1, ["1","2","3"])
+        readLine(L2, ["4","5","6"])
+        readLine(L3, ["7","8","9"])
+        readLine(L4, ["*","0","#"])
+        time.sleep(0.1)
+except KeyboardInterrupt:
+    print("\nApplication stopped!")
+
+# Servo part starts here
+
+servoPIN = 18
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servoPIN, GPIO.OUT)
+
+p = GPIO.PWM(servoPIN, 50)      # GPIO 18 for PWM with 50Hz
+p.start(2.5)                    # Initialization
+
+try:
+    while True:
+        p.ChangeDutyCycle(5)
+        time.sleep(0.5)
+        p.ChangeDutyCycle(7.5)
+        time.sleep(0.5)
+        p.ChangeDutyCycle(10)
+        time.sleep(0.5)
+        p.ChangeDutyCycle(12.5)
+        time.sleep(0.5)
+        p.ChangeDutyCycle(10)
+        time.sleep(0.5)
+        p.ChangeDutyCycle(7.5)
+        time.sleep(0.5)
+        p.ChangeDutyCycle(5)
+        time.sleep(0.5)
+        p.ChangeDutyCycle(2.5)
+        time.sleep(0.5)
+except KeyboardInterrupt:
+    p.stop()
+    GPIO.cleanup()
+
+# RFID Part Code Starts Here
+
+reader = SimpleMFRC522()
+
+print("Hold a tag near the reader")
+print("Reading tag in 1 second...")
+time.sleep(1)
+
+# Keep it for now. We only want it to run if the user calls for it from the browser UI.
+id = reader.read_id_no_block()
+
+if id:
+    print(hex(id))
+else:
+    print("No tag detected")
+    
+GPIO.cleanup()
+
 # Section for the Load Cell Code
 
 EMULATE_HX711=False
@@ -264,88 +369,3 @@ while True:
     except (KeyboardInterrupt, SystemExit):
         cleanAndExit()
 
-# Keypad part goes here. Remember to somehow get them into functions and even have properties
-# called via functions like with the picamera stuff. Also a password system.
-
-R1 = 5
-R2 = 6
-R3 = 13
-R4 = 19
-
-C1 = 26
-C2 = 20
-C3 = 21
-
-# Initialize the GPIO pins
-
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-
-GPIO.setup(R1, GPIO.OUT)
-GPIO.setup(R2, GPIO.OUT)
-GPIO.setup(R3, GPIO.OUT)
-GPIO.setup(R4, GPIO.OUT)
-
-# Make sure to configure the input pins to use the internal pull-down resistors
-
-GPIO.setup(C1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(C2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(C3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
-# The readLine function implements the procedure discussed in the article
-# It sends out a single pulse to one of the rows of the keypad
-# and then checks each column for changes
-# If it detects a change, the user pressed the button that connects the given line
-# to the detected column
-
-def readLine(line, characters):
-    GPIO.output(line, GPIO.HIGH)
-    if(GPIO.input(C1) == 1):
-        print(characters[0])
-    if(GPIO.input(C2) == 1):
-        print(characters[1])
-    if(GPIO.input(C3) == 1):
-        print(characters[2])
-    GPIO.output(line, GPIO.LOW)
-
-try:
-    while True:
-        # call the readLine function for each row of the keypad
-        readLine(L1, ["1","2","3"])
-        readLine(L2, ["4","5","6"])
-        readLine(L3, ["7","8","9"])
-        readLine(L4, ["*","0","#"])
-        time.sleep(0.1)
-except KeyboardInterrupt:
-    print("\nApplication stopped!")
-
-# Servo part starts here
-
-servoPIN = 17
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(servoPIN, GPIO.OUT)
-
-p = GPIO.PWM(servoPIN, 50)      # GPIO 17 for PWM with 50Hz
-p.start(2.5)                    # Initialization
-
-try:
-    while True:
-        p.ChangeDutyCycle(5)
-        time.sleep(0.5)
-        p.ChangeDutyCycle(7.5)
-        time.sleep(0.5)
-        p.ChangeDutyCycle(10)
-        time.sleep(0.5)
-        p.ChangeDutyCycle(12.5)
-        time.sleep(0.5)
-        p.ChangeDutyCycle(10)
-        time.sleep(0.5)
-        p.ChangeDutyCycle(7.5)
-        time.sleep(0.5)
-        p.ChangeDutyCycle(5)
-        time.sleep(0.5)
-        p.ChangeDutyCycle(2.5)
-        time.sleep(0.5)
-except KeyboardInterrupt:
-    p.stop()
-    GPIO.cleanup()
