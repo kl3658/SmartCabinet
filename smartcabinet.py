a25ae2dedc4f98106b934439fb359c119d39cbfb
Tommy Martinez <UGFruT@gmail.com>
2023-05-03 12:38:55 -0400

    Commented out the RFID portion. Reading is now MSB

--- smartcabinet.py
+++ smartcabinet.py
@@ -263,21 +263,21 @@ except KeyboardInterrupt:
 ''' Task 3 '''
 # RFID Part Code Starts Here
 
-reader = SimpleMFRC522()
+# reader = SimpleMFRC522()
 
-print("Hold a tag near the reader")
-print("Reading tag in 1 second...")
-time.sleep(1)
+# print("Hold a tag near the reader")
+# print("Reading tag in 1 second...")
+# time.sleep(1)
 
-# Keep it for now. We only want it to run if the user calls for it from the browser UI.
-id = reader.read_id_no_block()
+# # Keep it for now. We only want it to run if the user calls for it from the browser UI.
+# id = reader.read_id_no_block()
 
-if id:
-    print(hex(id))
-else:
-    print("No tag detected")
+# if id:
+#     print(hex(id))
+# else:
+#     print("No tag detected")
 
-GPIO.cleanup()
+# GPIO.cleanup()
 
 ''' Task 4 '''
 # Section for the Load Cell Code
@@ -303,7 +303,7 @@ def cleanAndExit():
 
 hx = HX711(5, 6) #GPIO Pins 5 and 6
 
-hx.set_reading_format("LSB", "MSB")
+hx.set_reading_format("MSB", "MSB")
 
 # HOW TO CALCULATE THE REFFERENCE UNIT
 # To set the reference unit to 1. Put 1kg on your sensor or anything you have and know exactly how much it weights.
