71db6421664334442b6c0a1fad30d88405f04d1c
Tommy Martinez <UGFruT@gmail.com>
2023-05-05 19:14:29 -0400

    Eliminated a function causing issues for Load Cell

--- smartcabinet.py
+++ smartcabinet.py
@@ -305,26 +305,6 @@ def rfidOperate():
 ''' Task 4 '''
 # Section for the Load Cell Code
 
-def loadcellSetup(hx, referenceUnit):
-
-    hx.set_reading_format("LSB", "MSB")
-
-    # HOW TO CALCULATE THE REFFERENCE UNIT
-    # To set the reference unit to 1. Put 1kg on your sensor or anything you have and know exactly how much it weights.
-    # In this case, 92 is 1 gram because, with 1 as a reference unit I got numbers near 0 without any weight
-    # and I got numbers around 184000 when I added 2kg. So, according to the rule of thirds:
-    # If 2000 grams is 184000 then 1000 grams is 184000 / 2000 = 92.
-    #hx.set_reference_unit(113)
-    hx.set_reference_unit(referenceUnit)
-
-    hx.reset()
-
-    hx.tare()
-
-    print("Tare done! Add weight now...")
-
-    return hx
-
 def cleanAndExit(EMULATE_HX711):
     print("Cleaning...")
 
@@ -347,8 +327,24 @@ def loadCellOperate():
         print("Emulated")
 
     hx = HX711(5, 6) # GPIO Pins 5 and 6
+    
+    hx.set_reading_format("LSB", "MSB")
 
-    hx = loadcellSetup(hx, referenceUnit)
+    # HOW TO CALCULATE THE REFFERENCE UNIT
+    # To set the reference unit to 1. Put 1kg on your sensor or anything you have and know exactly how much it weights.
+    # In this case, 92 is 1 gram because, with 1 as a reference unit I got numbers near 0 without any weight
+    # and I got numbers around 184000 when I added 2kg. So, according to the rule of thirds:
+    # If 2000 grams is 184000 then 1000 grams is 184000 / 2000 = 92.
+    #hx.set_reference_unit(113)
+    hx.set_reference_unit(referenceUnit)
+
+    hx.reset()
+
+    hx.tare()
+
+    print("Tare done! Add weight now...")
+
+    return hx
     
     while True:
         try:
