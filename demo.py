b625cbf8d5a8d4a1cc14835429cac0c314c87804
Tommy Martinez <UGFruT@gmail.com>
2023-05-05 16:41:38 -0400

    Testing out keypad now.

--- demo.py
+++ demo.py
@@ -1,13 +1,11 @@
 import smartcabinet
 
-smartcabinet.info()
-smartcabinet.cameraSetup()
-smartcabinet.turnOnCamera()
+#smartcabinet.info()
+#smartcabinet.cameraSetup()
+#smartcabinet.turnOnCamera()
 
 # Used to test out the identifyFace function
-smartcabinet.useCamera()
+#smartcabinet.useCamera()
 
-# We could test this function out 3 times, which calls identifyFace in turn
-#smartcabinet.savePhotoToFile(1)
-#smartcabinet.savePhotoToFile(2)
-#smartcabinet.savePhotoToFile(3)
+# Test out the keypad
+smartcabinet.keypadOperate()
