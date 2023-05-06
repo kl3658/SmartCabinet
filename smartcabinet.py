d4d7062c6c904f19073f3a1c20d538a5485beb81
Tommy Martinez <UGFruT@gmail.com>
2023-05-06 00:44:26 -0400

    Reverted keypadOperate

--- smartcabinet.py
+++ smartcabinet.py
@@ -205,7 +205,7 @@ def readLine(line, characters, C1, C2, C3):
             print("unlocked")
         else:
             print("Wrong key! Try again")
-
+            
     GPIO.output(line, GPIO.LOW)
 
 def keypadOperate():
@@ -219,14 +219,10 @@ def keypadOperate():
     try:
         while True:
             # call the readLine function for each row of the keypad
-            if (GPIO.input(R1) == 1):
-                readLine(R1, ["1","2","3"], C1, C2, C3)
-            elif (GPIO.input(R2) == 1):
-                readLine(R2, ["4","5","6"], C1, C2, C3)
-            elif (GPIO.input(R3) == 1):
-                readLine(R3, ["7","8","9"], C1, C2, C3)
-            elif (GPIO.input(R4) == 1):
-                readLine(R4, ["*","0","#"], C1, C2, C3)
+            readLine(R1, ["1","2","3"], C1, C2, C3)
+            readLine(R2, ["4","5","6"], C1, C2, C3)
+            readLine(R3, ["7","8","9"], C1, C2, C3)
+            readLine(R4, ["*","0","#"], C1, C2, C3)
             time.sleep(0.1)
     except KeyboardInterrupt:
         print("\nApplication stopped!")
