5a251c063e402aed381133db9fc727ba4221701d
Tommy Martinez <UGFruT@gmail.com>
2023-05-13 01:00:19 -0400

    Testing out threads now

--- demo.py
+++ demo.py
@@ -1,5 +1,7 @@
 import smartcabinet
+from threading import Thread
 
+'''
 print("Info going!")
 smartcabinet.info()
 print("Info gone!")
@@ -25,7 +27,19 @@ print("RFID gone!")
 print("Load Cell Incoming!")
 smartcabinet.loadCellOperate()
 print("Load Cell gone!")
+'''
 
 # We really only need about 4 threads. One for:
 # Camera, Keypad, RFID, and the Load Cell.
 # Servo isn't included as its called via other functions.
+cameraThread = Thread(target = smartcabinet.useCamera())
+cameraThread.start()
+
+keypadThread = Thread(target = smartcabinet.keypadOperate())
+keypadThread.start()
+
+rfidThread = Thread(target = smartcabinet.rfidOperate())
+rfidThread.start()
+
+loadCellThread = Thread(target = smartcabinet.loadCellOperate())
+loadCellThread.start()
