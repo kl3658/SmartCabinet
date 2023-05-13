f2e7fcc01d768c9349cb73452eb70d9863cf086a
Tommy Martinez <UGFruT@gmail.com>
2023-05-13 00:06:11 -0400

    Copied demo code into the app file

--- app.py
+++ app.py
@@ -101,3 +101,33 @@ def options_template():
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
+
+print("Info going!")
+smartcabinet.info()
+print("Info gone!")
+
+# Used to test out the camera
+smartcabinet.useCamera()
+
+# Test out the keypad
+print("Keypad coming!")
+smartcabinet.keypadOperate()
+print("Keypad Gone!")
+
+# Test out the servo
+print("Servo coming!")
+smartcabinet.servoOperate(1)
+print("Servo gone!")
+
+# Test out the RFID
+print("RFID coming!")
+smartcabinet.rfidOperate()
+print("RFID gone!")
+
+print("Load Cell Incoming!")
+smartcabinet.loadCellOperate()
+print("Load Cell gone!")
+
+# We really only need about 4 threads. One for:
+# Camera, Keypad, RFID, and the Load Cell.
+# Servo isn't included as its called via other functions.
