65ef19504c1ca79c9e68dcffa78af070c4d8466b
Tommy Martinez <UGFruT@gmail.com>
2023-05-13 01:33:17 -0400

    Updated threads to not pass functions themselves, but rather just the object

--- demo.py
+++ demo.py
@@ -32,13 +32,13 @@ print("Load Cell gone!")
 # We really only need about 4 threads. One for:
 # Camera, Keypad, RFID, and the Load Cell.
 # Servo isn't included as its called via other functions.
-cameraThread = Thread(target = smartcabinet.useCamera(), daemon=True)
+cameraThread = Thread(target = smartcabinet.useCamera)
 cameraThread.start()
-keypadThread = Thread(target = smartcabinet.keypadOperate(), daemon=True)
+keypadThread = Thread(target = smartcabinet.keypadOperate)
 keypadThread.start()
-rfidThread = Thread(target = smartcabinet.rfidOperate(), daemon=True)
+rfidThread = Thread(target = smartcabinet.rfidOperate)
 rfidThread.start()
-loadCellThread = Thread(target = smartcabinet.loadCellOperate(), daemon=True)
+loadCellThread = Thread(target = smartcabinet.loadCellOperate)
 loadCellThread.start()
 cameraThread.join()
 keypadThread.join()
